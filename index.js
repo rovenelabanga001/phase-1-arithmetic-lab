067c7935fbca6c71a85bb5dd3a78757f0d9f1a98
rovenelabanga <rovenelabanga2001@gmail.com>
2024-09-24 13:32:28 +0300

    manipulates various math operators and methods

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 28;
+const num4 = 32;
+const multiply = num1 * num2;
+const mod = num4 % num3;
+const max = Math.max(1,2,4,18,17,20, 5.5);
+
+const random = Math.ceil(Math.random());
+console.log(random);
